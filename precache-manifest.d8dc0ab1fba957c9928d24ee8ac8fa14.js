5bb36e9983d26ed86b8a75e43c93fbe10e7f03a7
m.suetin <m.suetin@freematiq.com>
2019-10-24 16:21:27 +0700

    Updates

---
+++ precache-manifest.d8dc0ab1fba957c9928d24ee8ac8fa14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b803e47df8947dc1a766f1dd8afef0",
+    "url": "/js-benchmark/index.html"
+  },
+  {
+    "revision": "31800f1503066f00eae6",
+    "url": "/js-benchmark/static/css/main.2fc38db4.chunk.css"
+  },
+  {
+    "revision": "61c74344617288e7db37",
+    "url": "/js-benchmark/static/js/2.ccebd40e.chunk.js"
+  },
+  {
+    "revision": "31800f1503066f00eae6",
+    "url": "/js-benchmark/static/js/main.928fe99d.chunk.js"
+  },
+  {
+    "revision": "b2184436cf80c79f60a7",
+    "url": "/js-benchmark/static/js/runtime-main.c2ac0d7a.js"
+  }
+]);
